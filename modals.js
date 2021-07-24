7fbee480ff1de62eaeb5886e82798e3741b39786
artursem <artur.semeniuk@gmail.com>
2021-07-24 14:48:21 +0200

    add modal styling

--- modals.js
+++ modals.js
@@ -2,6 +2,8 @@ const btn = document.querySelectorAll('.btn');
 const modalSelect = document.querySelector('.back');
 const modalSuccess = document.querySelector('.success');
 const background = document.querySelector('.background');
+const btnX = document.querySelector('.modal-close');
+
 
 function showSelect() {
     console.log('modal select');
@@ -13,4 +15,18 @@ function showSuccess() {
     console.log('modal success');
 }
 
+function hideModal() {
+    modalSelect.classList.add('hide');
+    modalSuccess.classList.add('hide');
+    background.classList.add('hide');
+}
+
 btn.forEach(btn => btn.addEventListener('click', showSelect));
+btnX.addEventListener('click', hideModal);
+window.addEventListener('click', (e) => {
+    // if ANY modal !hide
+    // and click outside modal
+    // hideModal();
+    
+    console.log(e.target);
+})
