031c7103d5d4015d214fc9f764505acd06998aea
artursem <artur.semeniuk@gmail.com>
2021-07-24 12:27:03 +0200

    add bookmark animation

--- bookmark.js
+++ bookmark.js
@@ -1,4 +1,5 @@
 const btnBookmark = document.querySelector('.btn-bookmark');
+const text = document.querySelector('.btn-bookmark-text');
 let isBookmarked = false;
 
 
@@ -7,11 +8,13 @@ function handleClick() {
         btnBookmark.classList.remove('btn-on');
         btnBookmark.classList.add('btn-off');
         btnBookmark.style.flexDirection = 'row';
+        text.textContent = 'Bookmark';
         return isBookmarked = false;
     } else {
         btnBookmark.classList.remove('btn-off');
         btnBookmark.classList.add('btn-on');
         btnBookmark.style.flexDirection = 'row-reverse';
+        text.textContent = 'Bookmarked';
         return isBookmarked = true;
     } 
 }
