5fdfced08e3f4a73853346683304ce4201afe53d
artursem <artur.semeniuk@gmail.com>
2021-07-31 16:04:36 +0200

    add mobile menu

---
+++ menu.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('.header__nav__hamburger');
+const icon = btn.querySelector('img');
+const menu = document.querySelector('ul');
+
+
+let isMenuOpen = false;
+
+function showMenu() {
+    icon.src = './images/icon-close-menu.svg';
+    menu.style.display = 'flex';
+    background.classList.remove('hide');
+    isMenuOpen = true;
+    return isMenuOpen;
+}
+
+function hideMenu() {
+    icon.src = './images/icon-hamburger.svg';
+    menu.style.display = 'none';
+    background.classList.add('hide');
+    isMenuOpen = false;
+    return isMenuOpen;
+}
+function menuClick() {
+    if (isMenuOpen) {
+        hideMenu();
+    } else {
+        showMenu();
+    }
+}
+
+btn.addEventListener('click', menuClick);
+background.addEventListener('click', hideMenu);
+menu.addEventListener('click', hideMenu);
